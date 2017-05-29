8e934fb2e3f5735659ee6dfb4727701b6e62a0ae
Muz <smuzani@gmail.com>
2017-05-29 16:46:32 +0800

    Facebook login

--- app/src/main/java/com/syedmuzani/umbrella/activities/LoginActivity.kt
+++ app/src/main/java/com/syedmuzani/umbrella/activities/LoginActivity.kt
@@ -1,21 +1,48 @@
 package com.syedmuzani.umbrella.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import com.facebook.FacebookSdk
-import com.facebook.appevents.AppEventsLogger
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.syedmuzani.umbrella.R
+import org.jetbrains.anko.toast
+
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var callbackManager: CallbackManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
+
+        callbackManager = CallbackManager.Factory.create()
+        LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(loginResult: LoginResult) {
+                        toast("Success! " + loginResult.toString())
+                    }
+
+                    override fun onCancel() {
+                        toast("Canceled")
+                    }
+
+                    override fun onError(exception: FacebookException) {
+                        toast("Error: " + exception.toString())
+                    }
+                })
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
 }
--- app/src/main/java/com/syedmuzani/umbrella/fragments/LoginFragment.kt
+++ app/src/main/java/com/syedmuzani/umbrella/fragments/LoginFragment.kt
@@ -1,12 +1,19 @@
 package com.syedmuzani.umbrella.fragments
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
 import com.syedmuzani.umbrella.R
+import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -15,6 +22,29 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.fragment_login, container, false)
+
+        val v = inflater!!.inflate(R.layout.fragment_login, container, false)
+        var loginButton: LoginButton = v.find(R.id.login_button);
+        loginButton.setReadPermissions("email")
+        // If using in a fragment
+        loginButton.fragment = this
+        // Other app specific specialization
+
+        // Callback registration
+        val callbackManager = CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                context.toast("Success! " + loginResult.toString())
+            }
+
+            override fun onCancel() {
+                context.toast("Canceled")
+            }
+
+            override fun onError(exception: FacebookException) {
+                context.toast("Error: " + exception.toString())
+            }
+        })
+        return v
     }
 }
