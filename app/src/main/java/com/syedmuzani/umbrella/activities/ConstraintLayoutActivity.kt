93a286215f8b076b8198877ca047e8b384dfa3cf
Muz <smuzani@gmail.com>
2017-06-11 17:32:03 +0800

    WIP: Constraints and best practice

---
+++ app/src/main/java/com/syedmuzani/umbrella/activities/ConstraintLayoutActivity.kt
@@ -0,0 +1,13 @@
+package com.syedmuzani.umbrella.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.syedmuzani.umbrella.R
+
+class ConstraintLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
---
+++ app/src/main/java/com/syedmuzani/umbrella/activities/DesignPatternsActivity.kt
@@ -0,0 +1,17 @@
+package com.syedmuzani.umbrella.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.syedmuzani.umbrella.R
+
+/**
+ * https://github.com/dbacinski/Design-Patterns-In-Kotlin
+ */
+
+class DesignPatternsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_design_patterns)
+    }
+}
