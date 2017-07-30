1790b9ddf08077c40a54fdfaaf1611eea8a047b3
Muz <smuzani@gmail.com>
2017-07-30 09:57:18 +0800

    Fingerprint

---
+++ app/src/main/java/com/syedmuzani/umbrella/activities/FingerprintActivity.java
@@ -0,0 +1,15 @@
+package com.syedmuzani.umbrella.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.syedmuzani.umbrella.R;
+
+public class FingerprintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fingerprint);
+    }
+}
---
+++ app/src/main/java/com/syedmuzani/umbrella/utils/FingerprintHandler.java
@@ -0,0 +1,8 @@
+package com.syedmuzani.umbrella.utils;
+
+/**
+ * Created by Muz on 2017-07-30.
+ */
+
+public class FingerprintHandler {
+}
