4e2d467cb21ad891774862e168dfa6a3e4e1d326
Muz <smuzani@gmail.com>
2017-09-05 15:12:58 +0800

    Display the timer status

--- app/src/main/java/com/syedmuzani/umbrella/activities/FirebaseActivity.kt
+++ app/src/main/java/com/syedmuzani/umbrella/activities/FirebaseActivity.kt
@@ -2,20 +2,26 @@ package com.syedmuzani.umbrella.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.syedmuzani.umbrella.R
 import com.syedmuzani.umbrella.models.FirebaseTimestamp
+import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
+import java.sql.Timestamp
 
 
 class FirebaseActivity : AppCompatActivity() {
 
+    lateinit var tvStatus: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_firebase)
+        tvStatus = find(R.id.tv_status)
         setupDatetime()
     }
 
@@ -26,10 +32,7 @@ class FirebaseActivity : AppCompatActivity() {
         refDate.child("end").setValue(1507637532)
         refDate.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                val timer = dataSnapshot.getValue(FirebaseTimestamp::class.java)
-                toast("Start: " + timer.start + " and end: " + timer.end)
+                displayData(dataSnapshot)
             }
 
             override fun onCancelled(error: DatabaseError) {
@@ -37,6 +40,23 @@ class FirebaseActivity : AppCompatActivity() {
                 toast("Failed to read value: " + error.toException())
             }
         })
+    }
+
+    private fun displayData(dataSnapshot: DataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        val timer = dataSnapshot.getValue(FirebaseTimestamp::class.java)
+        val tStart = Timestamp(timer.start * 1000)
+        val tEnd = Timestamp(timer.end * 1000)
+        val tNow = Timestamp(System.currentTimeMillis())
+
+        if (tNow < tStart) {
+            tvStatus.text = "Time has not yet started"
+        } else if (tNow >= tStart && tNow < tEnd) {
+            tvStatus.text = "Time is happening"
+        } else {
+            tvStatus.text = "Time ended"
+        }
     }
 
     private fun test() {
